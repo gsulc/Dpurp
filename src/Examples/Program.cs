8cabfda493e4529b3d3ee6125e5a019d8551b976
gsulc <gordon.sulc@gmail.com>
2018-07-22 22:44:18 -0400

    example uses unity
    now the example uses unity framework to swap between different repository types

--- src/Examples/Program.cs
+++ src/Examples/Program.cs
@@ -1,27 +1,46 @@
 ﻿using Dpurp.Abstractions;
+using Dpurp.EntityFramework;
 using Dpurp.Json;
+using Dpurp.Xml;
 using Examples.Model;
 using System;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Unity;
+using Unity.Injection;
 
 namespace Examples
 {
     class Program
     {
-        private static readonly string executingPath = 
-            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        static void Main(string[] args)
+        enum DataProviderType
         {
-            TestBlogWithJson();
+            SqlServer,
+            XmlFileStore,
+            JsonFileStore,
         }
 
-        static void TestBlogWithJson()
+        class ApplicationSettings
         {
-            var jsonContext = new JsonFileContext(executingPath);
-            IRepository<Blog> blogRepository = new JsonFileRepository<Blog>(jsonContext);
-            IRepository<Post> postRepository = new JsonFileRepository<Post>(jsonContext);
+            public DataProviderType DataProvider;
+            public string ConnectionString;
+        }
+
+        private static readonly string executingPath = 
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        static void Main(string[] args)
+        {
+            var appSettings = new ApplicationSettings
+            {
+                DataProvider = DataProviderType.XmlFileStore,
+                ConnectionString = executingPath
+            };
+            
+            IRepository<Blog> blogRepository = CreateRepo<Blog>(appSettings);
+            IRepository<Post> postRepository = CreateRepo<Post>(appSettings);
 
             // Create and save a new Blog 
             Console.Write("Enter a name for a new Blog: ");
@@ -40,6 +59,41 @@ namespace Examples
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        static IRepository<T> CreateRepo<T>(ApplicationSettings settings) where T : class
+        {
+            using (var container = new UnityContainer())
+            {
+                container.RegisterInstance(settings);
+
+                switch (settings.DataProvider)
+                {
+                    case DataProviderType.SqlServer:
+                        container.RegisterType<DbContext, EntityDataContext>();
+                        container.RegisterType<EntityDataContext>(
+                            new InjectionConstructor(settings.ConnectionString));
+                        container.RegisterType(typeof(IRepository<>), typeof(EntityRepository<>));
+                        break;
+
+                    case DataProviderType.XmlFileStore:
+                        container.RegisterType<XmlFileContext>(
+                            new InjectionConstructor(settings.ConnectionString));
+                        container.RegisterType(typeof(IRepository<>), typeof(XmlFileRepository<>));
+                        break;
+
+                    case DataProviderType.JsonFileStore:
+                        container.RegisterType<JsonFileContext>(
+                            new InjectionConstructor(settings.ConnectionString));
+                        container.RegisterType(typeof(IRepository<>), typeof(JsonFileRepository<>));
+                        break;
+
+                    default:
+                        throw new Exception($"The DataProvider Type {settings.DataProvider} is unknown");
+                }
+
+                return container.Resolve<IRepository<T>>();
+            }
         }
     }
 }
